a8342ec433574b478151be6b840fd7ef3869698b
maxoly <massimo.oliviero@gmail.com>
2012-05-22 21:49:14 +0200

    xcode project empty template

---
+++ PMStarterKit/PMStarterKit/PMSKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMSKAppDelegate.h
+//  PMStarterKit
+//
+//  Created by Massimo Oliviero on 5/22/12.
+//  Copyright (c) 2012 pragmamark.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMSKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PMStarterKit/PMStarterKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PMStarterKit
+//
+//  Created by Massimo Oliviero on 5/22/12.
+//  Copyright (c) 2012 pragmamark.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMSKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMSKAppDelegate class]));
+    }
+}
