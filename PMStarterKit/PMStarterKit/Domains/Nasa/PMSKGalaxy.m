dc3c3dce87df1ca392741abda7df8d3ce9e7a88e
maxoly <massimo.oliviero@gmail.com>
2012-05-25 00:37:38 +0200

    new domains folder

--- PMStarterKit/PMStarterKit/Domains/Nasa/PMSKGalaxy.m
+++ PMStarterKit/PMStarterKit/Domains/Nasa/PMSKGalaxy.m
@@ -8,6 +8,40 @@
 
 #import "PMSKGalaxy.h"
 
+@interface PMSKGalaxy ()
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, retain) NSArray *planets;
+
+@end
+
 @implementation PMSKGalaxy
+
+
+- (id)initWithName:(NSString *)name
+{
+    self = [super init];
+    
+    if(self)
+    {
+        self.name = name;
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [_name release];
+    [_planets release];
+    [super dealloc];
+}
+
+
+
+#pragma mark - Properties
+
+@synthesize name    = _name;
+@synthesize planets  = _planets;
 
 @end
